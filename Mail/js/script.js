ef5b3500ed8da8c9b1f4990e3da1cf0be64dd67b
Simone Romiti <sr1992ei@gmail.com>
2020-11-12 17:25:58 +0100

    Esercizio email completo

--- Mail/js/script.js
+++ Mail/js/script.js
@@ -1,23 +1,29 @@
 // Chiedi all’utente la sua email,
 // controlla che sia nella lista di chi può accedere,
 // stampa un messaggio appropriato sull’esito del controllo.
+function myFunction(){
 
-var listaMail = ["pinco.pallino@gmail.com", "nome.cognome@gmail.com", "sr1992ei@gmail.com", "simone.romiti@gmail.com"];
+  var listaMail = ["pinco.pallino@gmail.com", "nome.cognome@gmail.com", "sr1992ei@gmail.com", "simone.romiti@gmail.com"];
 
-var emailUtente = prompt("Inserisci la tua e-mail");
-var i;
-var answer = 0;
+  var emailUtente = document.getElementById("input").value;
+  console.log(emailUtente);
+  var i;
+  var answer = 0;
 
-for(i = 0; i < listaMail.length; i++) {
-  if(emailUtente == listaMail[i]) {
-    answer = 1;
-    i = 3;
+  for(i = 0; i < listaMail.length; i++) {
+    if(emailUtente == listaMail[i]) {
+      answer = 1;
+      i = 3;
+    }
   }
-}
 
-if(answer == 1) {
-  alert("email OK");
-} else {
-  alert("e-mail non registrata");
+  if(answer == 1) {
+    document.getElementById("control").style.color = "green";
+    document.getElementById("control").innerHTML = "Accesso consentito!";
+  } else {
+    document.getElementById("control").style.color = "red";
+    document.getElementById("control").innerHTML = "Accesso negato!";
+  }
+  console.log(answer);
+
 }
-console.log(answer);
